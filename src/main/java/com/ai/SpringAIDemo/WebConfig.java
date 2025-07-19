918e0178a662c92da152105f90846474b2e0fdbc
Atharv_7 <atharvwavare3563@gmail.com>
2025-07-19 13:43:43 +0530

    new files

--- src/main/java/com/ai/SpringAIDemo/WebConfig.java
+++ src/main/java/com/ai/SpringAIDemo/WebConfig.java
@@ -3,7 +3,6 @@ package com.ai.SpringAIDemo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000","http://192.168.149.196:3000")
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
