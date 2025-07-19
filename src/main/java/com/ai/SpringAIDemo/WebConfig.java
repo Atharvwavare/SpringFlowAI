34d984e6458001283979510993cde41ec01ac0f5
Atharv_7 <atharvwavare3563@gmail.com>
2025-07-19 14:27:44 +0530

    changed files

--- src/main/java/com/ai/SpringAIDemo/WebConfig.java
+++ src/main/java/com/ai/SpringAIDemo/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000","https://spring-frontend-c8r45pwb2-atharvwavares-projects.vercel.app")
                         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
